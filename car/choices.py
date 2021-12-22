430a4260a81a78dc97c76b30d5b238f89750c0b1
rishabh <rishabhsenger7530@gmail.com>
2021-12-22 12:09:19 +0530

    second commit

--- car/choices.py
+++ car/choices.py
@@ -1,10 +1,8 @@
-
 CATEGORY_CHOICES = (
     ("Economy", "Economy"),
     ("Business", "Business"),
     ("First_Class", "First_Class)"),
- 
-)
+ )
 
 CAR_MOTOR_CHOICES = (
     ("Hybrid", "Hybrid"),
